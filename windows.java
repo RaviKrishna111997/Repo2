78c05b9c2f427938f5109ca32c9118fa5f299e6f
RaviKrishna111997 <krishnakris99999@gmail.com>
2025-08-25 18:17:20 +0530

    Update windows.java

---
+++ windows.java
@@ -0,0 +1,5 @@
+
+In Java, any line starting with // is a comment. Comments are intended for users reading the code to understand the intent and functionality of the program. It is completely ignored by the Java compiler (an application that translates Java program to Java bytecode that computer can execute). To learn more, visit Java comments.
+class HelloWorld { ... }
+
+In Java, every application begins with a
